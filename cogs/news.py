3a7a50cf0fbc45007eb6b106ddaf6d64662e26e9
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-12 08:15:25 +0000

    ok

--- cogs/news.py
+++ cogs/news.py
@@ -1,1 +1,15 @@
-from 
+from discord.ext import commands
+from lib.news import News
+
+
+class News(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def news(self, ctx):
+        pass
+
+
+async def setup(bot):
+    await bot.add_cog(News(bot))
