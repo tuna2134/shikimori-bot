4b5b1dc2fbbf661849a3590e39602bf1754c5fcc
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-12 22:27:35 +0900

    Update news.py

--- cogs/news.py
+++ cogs/news.py
@@ -20,7 +20,7 @@ class News(commands.Cog):
                 await cursor.execute("CREATE TABLE IF NOT EXISTS News(link TEXT);")
                 await cursor.execute("CREATE TABLE IF NOT EXISTS NewsChannel(channelid BIGINT);")
                 await cursor.execute("SELECT * FROM News LIMIT 1;")
-                self.last = await cursor.fetchone()[0]
+                self.last = (await cursor.fetchone())[0]
                 await cursor.execute("SELECT * FROM NewsChannel;")
                 self.channelids = [channelid for (channelid,) in await cursor.fetchall()]
         self.notice.start()
