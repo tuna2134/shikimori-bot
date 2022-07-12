82cad3877bc8ed541c1ca2d9da8ac487222ffc4d
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-12 22:36:52 +0900

    Update page.py

--- util/page.py
+++ util/page.py
@@ -28,7 +28,7 @@ class Page(ui.View):
         await self.update(interaction)
         
     async def update(self, interaction):
-        await interaction.message.edit(embed=self.pages[self.current_page])
+        await interaction.response.edit_message(embed=self.pages[self.current_page])
         
     @property
     def first(self):
