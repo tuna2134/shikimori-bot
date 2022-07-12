0a9719706d20962bb4d4552c5ffb13e982c3cd34
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-13 08:32:47 +0900

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -1,17 +1,31 @@
 from discord.ext import commands
 
+from .news import Embed
+
+from inspect import cleandoc
+
 
 class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(extras={
+        "args": []
+    }, description="pingコマンド")
     async def ping(self, ctx):
-        await ctx.send(f"Pong! `{self.bot.latency}`ms")
+        await ctx.send(f"Pong! `{round(self.bot.latency * 1000, 1)}`ms")
 
-    @commands.command()
+    @commands.command(extras={
+        "args": []
+    }, description="Infoコマンド")
     async def info(self, ctx):
-        await ctx.send(f"ShikimoriBot v{self.bot.version}")
+        await ctx.send(embed=Embed(title="Information", description=cleandoc(f"""
+        Version: v{self.bot.version}
+        
+        このbotは「可愛いだけじゃない式守さん」を応援するために作られたボットです。
+　　　　　著作権者などにbot開発中止命令及び提供中止命令が出た場合、それに従いますので、悪しからずご了承ください。
+     　　
+        ※著作権者はこの場合式守さん制作委員会及び真木圭吾さんのことを表します。"""))
 
 async def setup(bot):
     await bot.add_cog(Main(bot))
